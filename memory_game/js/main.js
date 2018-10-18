95b8cc61b2b857f9e581fe123b24ef823f5166a6
Eshan Misra <eshanmisra@gmail.com>
2018-10-18 13:23:04 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+},
+]
 
 var cardsInPlay = [];
 
@@ -13,15 +34,17 @@ console.log("Sorry, try again.");
 }
 
 var flipCard = function (cardId) {
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch()
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
 
+console.log(cardImage.rank);
+console.log(suit.rank);
 
 }
 
