d043535f73b661f8595000362a4f023cdbfe0db0
Eshan Misra <eshanmisra@gmail.com>
2018-10-18 10:19:57 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,21 +4,26 @@ var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay);
-
-
-if (cardsInPlay.length === "2") {
-} else if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else if (cardOne !== cardTwo) {
-	alert("Sorry, try again.");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
+}
+
+var flipCard = function (cardId) {
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+checkForMatch()
+
+flipCard(0);
+flipCard(2);
+
+
+}
+
+
 
